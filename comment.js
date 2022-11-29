1b22b941ae8ba6168408ed2aa52a2c90317229aa
CloudOh <106655585+CloudOh@users.noreply.github.com>
2022-11-29 09:37:52 +0900

    Add files via upload

--- comment.js
+++ comment.js
@@ -3,24 +3,24 @@ const commentsContainer = document.querySelector("#comments");
 
 // 댓글 작성
 instaForm.addEventListener("submit", function (e) {
-            e.preventDefault();
-            const usernameInput = instaForm.elements.username;
-            const commentInput = instaForm.elements.comment;
-            addComment(usernameInput.value, commentInput.value);
-            usernameInput.value = "";
-            commentInput.value = "";
-            });
+    e.preventDefault();
+    const usernameInput = instaForm.elements.username;
+    const commentInput = instaForm.elements.comment;
+    addComment(usernameInput.value, commentInput.value);
+    usernameInput.value = "";
+    commentInput.value = "";
+});
 
-            const addComment = (username, comment) => {
-            const newComment = document.createElement("li");
-            const bTag = document.createElement("b");
-            bTag.append(username);
-            newComment.append(bTag);
-            newComment.append(`- ${comment}`);
-            commentsContainer.append(newComment);
-            };
+const addComment = (username, comment) => {
+    const newComment = document.createElement("li");
+    const bTag = document.createElement("b");
+    bTag.append(username);
+    newComment.append(bTag);
+    newComment.append(`- ${comment}`);
+    commentsContainer.append(newComment);
+};
 
-            // 댓글 삭제
-            commentsContainer.addEventListener("click", (e) => {
-            e.target.nodeName === "LI" && e.target.remove();
-            });
+// 댓글 삭제
+commentsContainer.addEventListener("click", (e) => {
+    e.target.nodeName === "LI" && e.target.remove();
+});
